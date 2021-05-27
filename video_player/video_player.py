b3bf1df1a6c286a5a5dafad97595c736b205c693
AK HACKS OR TRICKS <71229713+AadityaKandel@users.noreply.github.com>
2021-05-27 13:27:53 +0545

    Update video_player.py
    I dont know open cv but i added some tkinter in your python script and made it like an app. But there is only one error. If i drag the video window the sound continues but video stucks. Because of that sound and video doesn't play at the same time. I cant solve that problem. So if you can, please make another update for that.

--- video_player/video_player.py
+++ video_player/video_player.py
@@ -2,59 +2,67 @@ import cv2
 import os
 from pathlib import Path
 from ffpyplayer.player import MediaPlayer
+from tkinter import filedialog
+from tkinter import *
+import tkinter.messagebox as msg
+# New Way Of Opening File
+
+root = Tk()
+root.title('Video_Player By Deepak Raj')
+root.geometry('400x200')
+
+def ask_dir():
+
+    b1.config(text="Exit Video",command=exitter)
+
+    video_directory_path = filedialog.askopenfilename(initialdir="")
+
+    def PlayVideo():
+
+        video = cv2.VideoCapture(video_directory_path)
+        player = MediaPlayer(video_directory_path)
+
+        while True:
+            root.update()
+            grabbed, frame = video.read()
+            audio_frame, val = player.get_frame()
+            if not grabbed:
+                msg.showwarning('Warning',"Video File Is Needed")
+                os.system('cls')
+                break
+            if cv2.waitKey(28) & 0xFF == ord("q"):
+                break
+            cv2.imshow("Video", frame)
+            if val != "eof" and audio_frame is not None:
+                img, t = audio_frame
+        video.release()
+        cv2.destroyAllWindows()
+
+    try:
+
+        PlayVideo()
+
+    except:
+
+        msg.showwarning('Warning',"Video File Is Needed")
+
+def exitter():
+    os.system('taskkill /PID python.exe /F')
+    b1.config(text="Play Video",command=ask_dir)
+
+def emp():
+    Label(bg='black',fg="black").pack()
+
+emp()
+emp()
+emp()    
+
+b1 = Button(text="Play Video",bg="white",fg="black",font="comicsansms 20 italic",relief=SUNKEN,command=ask_dir,justify=CENTER)
+b1.pack()
 
 
-# User input for the name of the image file.
-video_name = input("Name of the video file that you want to play:    ")
-
-# User input for the path of the image file.
-video_directory_guess = input("Directory that may contain the video:    ")
+# print (video_directory_path)
 
 
-# This function finds your file. If you don't know the directory just type '/'
-def find_the_video(file_name, directory_name):
-    files_found = []
-    for path, subdirs, files in os.walk(directory_name):
-        for name in files:
-            if file_name == name:
-                file_path = os.path.join(path, name)
-                files_found.append(file_path)
-
-    print(files_found)
-    return files_found[0]  # Return the path.
-
-
-# Initialize the path of the image file.
-video_directory = Path(find_the_video(video_name, video_directory_guess))
-
-# Initialize the parent directory of the image path.
-new_working_directory = video_directory.parent
-
-# Change the working directory of the script.
-os.chdir(new_working_directory)
-
-
-video_path = find_the_video(video_name, video_directory_guess)
-
-
-def PlayVideo(video_path):
-
-    video = cv2.VideoCapture(video_path)
-    player = MediaPlayer(video_path)
-
-    while True:
-        grabbed, frame = video.read()
-        audio_frame, val = player.get_frame()
-        if not grabbed:
-            print("End of video")
-            break
-        if cv2.waitKey(28) & 0xFF == ord("q"):
-            break
-        cv2.imshow("Video", frame)
-        if val != "eof" and audio_frame is not None:
-            img, t = audio_frame
-    video.release()
-    cv2.destroyAllWindows()
-
-
-PlayVideo(video_path)
+root.config(bg="black")
+root.mainloop()
